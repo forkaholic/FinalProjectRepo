481675aded12b8134035660373ba4f6d70de4fa7
salterdatwit <54985588+salterdatwit@users.noreply.github.com>
2019-10-28 11:51:47 -0400

    Create Calculation.java

---
+++ src/edu/wit/cs/comp1050/Calculation.java
@@ -0,0 +1,12 @@
+//Late as Always
+import java.util.ArrayList;
+
+public class Calculations {
+  private ArrayList<Character> chars;
+  public Calculations(ArrayList<Character> chars) {
+    this.chars = chars;
+  }
+  public double calculate() {
+    return ...;
+  }  
+}

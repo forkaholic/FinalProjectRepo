61c8ff670896c0f7d762cc16906e47282fb90563
Devin Salter <salterd@wit.edu>
2019-10-30 11:41:18 -0400

    Basic get and set for result

--- src/edu/wit/cs/comp1050/BasicCalculator.java
+++ src/edu/wit/cs/comp1050/BasicCalculator.java
@@ -1,12 +1,15 @@
 package edu.wit.cs.comp1050;
 import java.util.ArrayList;
-
+//Late as Always
 @SuppressWarnings("serial")
 public class BasicCalculator extends ArrayList<Calculation> {
+	
+	private double lastResult;
+	
 	public BasicCalculator() {}
 	
 	/**
-	 * This class adds two numbers together.
+	 * This method adds two numbers together.
 	 * @param num1 left hand number
 	 * @param num2 right hand number
 	 * @return sum
@@ -16,7 +19,7 @@ public class BasicCalculator extends ArrayList<Calculation> {
 	}
 	
 	/**
-	 * This class subtracts the second number from the first.
+	 * This method subtracts the second number from the first.
 	 * @param num1 left hand number
 	 * @param num2 right hand number
 	 * @return difference
@@ -26,7 +29,7 @@ public class BasicCalculator extends ArrayList<Calculation> {
 	}
 	
 	/**
-	 * This class multiplies two numbers together.
+	 * This method multiplies two numbers together.
 	 * @param num1 left hand number
 	 * @param num2 right hand number
 	 * @return product
@@ -36,13 +39,29 @@ public class BasicCalculator extends ArrayList<Calculation> {
 	}
 	
 	/**
-	 * This class divides the first number by the second, but checks if the second is zero before dividing. This class also must be return type Double instead of primitive type double, as the primitive cannot be returned as null.
+	 * This method divides the first number by the second, but checks if the second is zero before dividing. This class also must be return type Double instead of primitive type double, as the primitive cannot be returned as null.
 	 * @param num1 left hand number
 	 * @param num2 right hand number
 	 * @return either quotient or null
 	 */
 	public Double divide(double num1, double num2) {
 		return (num2 != 0) ? (num1 / num2) : null;
+	}
+	
+	/**
+	 * This method saves the result of the previous calculation.
+	 * @param num lastResult
+	 */
+	public void setResult(double num) {
+		this.lastResult = num;
+	}
+	
+	/**
+	 * This method sets lastResult as the previous calculation's value.
+	 * @return lastResult
+	 */
+	public double getResult() {
+		return this.lastResult;
 	}
 	
 	public void start() {

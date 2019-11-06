68e860ce015ec63c806aec25a79fcf04254630d7
Devin Salter <salterd@wit.edu>
2019-11-06 12:04:10 -0500

    CalculatorGUI updated

--- src/edu/wit/cs/comp1050/CalculatorGUI.java
+++ src/edu/wit/cs/comp1050/CalculatorGUI.java
@@ -2,7 +2,10 @@ package edu.wit.cs.comp1050;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 //Late as Always
@@ -28,6 +31,11 @@ public class CalculatorGUI extends Application{
 		Button zero = new Button("0");
 		
 		//FUNCTIONS		
+		Button toggle = new Button("Toggle Mode");
+		Button equals = new Button("=");
+		Button openP = new Button("(");
+		Button closeP = new Button(")");
+		
 		Button addition= new Button("+");
 		Button subtraction = new Button("-");
 		Button multiplication = new Button("*");
@@ -39,20 +47,23 @@ public class CalculatorGUI extends Application{
 		Button sine = new Button("sin(x)");
 		Button cosine = new Button("cos(x)");
 		
-		
-		
+		//PANES**********************************
+		//SCREEN
+		Text screen = new Text();
+	
+		//BASIC
 		Pane basic = new Pane();
-		
+		VBox base = new VBox();
+		VBox science = new VBox();
+		HBox row1 = new HBox();
+		HBox row2 = new HBox();
+		HBox row3 = new HBox();
+		HBox row4 = new HBox();
+		HBox row5 = new HBox();
+				
 		Pane scientific = new Pane();
-
-		Button swtch = new Button();
 		
-		
-		
-		
-		
-		
-		swtch.setOnAction(event -> {
+		toggle.setOnAction(event -> {
 			if(basic.isVisible()) {
 				primaryStage.setTitle("Scientific Calculator");
 				primaryStage.setScene(new Scene(scientific, 200, 200));

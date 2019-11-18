fd8ddd8e5277b792e142275d07f3162fe8d42e20
Devin Salter <salterd@wit.edu>
2019-11-18 10:49:16 -0500

    ScientificCalculator - All methods Added

--- src/edu/wit/cs/comp1050/ScientificCalculator.java
+++ src/edu/wit/cs/comp1050/ScientificCalculator.java
@@ -3,13 +3,60 @@ package edu.wit.cs.comp1050;
 @SuppressWarnings("serial")
 public class ScientificCalculator extends BasicCalculator{
 	public ScientificCalculator() {}
-	/*
-	 * Title: Base-2 and Base-N logarithm calculation in Java
-	 * Author: giannis
-	 * Date: January 9, 2010
-	 * Availability: https://www.giannistsakiris.com/2010/01/09/base-2-and-base-n-logarithm-calculation-in-java/ 
+
+	/**
+	 * This method returns log of given base and number
+	 * @param num
+	 * @param base
+	 * @return log(base)num
 	 */
 	public double log(double num, double base) {
 		return Math.log10(num) / Math.log10(base);
+	}
+	
+	/**
+	 * This method takes the square root of a number
+	 * @param num
+	 * @return sqrt(num)
+	 */
+	public double sqrt(double num) {
+		return Math.sqrt(num);
+	}
+	
+	/**
+	 * This method takes the sine of a given angle
+	 * @param angle
+	 * @return sin(angle)
+	 */
+	public double sin(double angle) {
+		return Math.sin(angle);
+	}
+	
+	/**
+	 * This method takes the cosine of a given angle
+	 * @param angle
+	 * @return cos(angle)
+	 */
+	public double cos(double angle) {
+		return Math.cos(angle);
+	}
+	
+	/**
+	 * This method finds e^x
+	 * @param power
+	 * @return e^x
+	 */
+	public double exp(double power) {
+		return Math.exp(power);
+	}
+	
+	/**
+	 * This method finds base^power
+	 * @param base
+	 * @param power
+	 * @return base^power
+	 */
+	public double pow(double base, double power) {
+		return Math.pow(base, power);
 	}
 }

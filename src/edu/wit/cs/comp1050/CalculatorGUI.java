f2d0ecde2a5b3687a726abd044865565d5975e59
Devin Salter <salterd@wit.edu>
2019-11-18 19:14:58 -0500

    CalculatorGUI updated

--- src/edu/wit/cs/comp1050/CalculatorGUI.java
+++ src/edu/wit/cs/comp1050/CalculatorGUI.java
@@ -1,5 +1,7 @@
 package edu.wit.cs.comp1050;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -13,52 +15,52 @@ import javafx.stage.Stage;
 public class CalculatorGUI extends Application{
 	
 	private boolean scientific;
+
+	private StringBuilder currentCalc = new StringBuilder();
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
 	
 	@SuppressWarnings("unused")
-	public void start(Stage primaryStage) throws Exception {		
+	public void start(Stage primaryStage) throws Exception {
+		ScientificCalculator storage = new ScientificCalculator();
+		Text screen = new Text(currentCalc.toString());
 		//BUTTONS*****************************************
-		//NUMBERS
-		Button one = new Button("1");
-		Button two = new Button("2");
-		Button three = new Button("3");
-		Button four = new Button("4");
-		Button five = new Button("5");
-		Button six = new Button("6");
-		Button seven = new Button("7");
-		Button eight = new Button("8");
-		Button nine = new Button("9");
-		Button zero = new Button("0");
+		/**
+		 * DVarga, 2016, Source Code. https://stackoverflow.com/questions/40967789/check-which-button-object-is-clicked-using-javafx
+		 */		
+		String[] strings = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "ANS", ".",
+				"_", "(", ")", "+", "-", "*", "/", "log",
+				"\u221A(", "^(", "e^(", "sin(", "cos("};
+		Button[] buttons = new Button[strings.length];
+		for(int i = 0; i < strings.length; i++) {
+			final int buttonID = i;
+			buttons[buttonID] = new Button(strings[buttonID]);
+			buttons[buttonID].setMinSize(150, 50);
+			buttons[buttonID].setOnAction(e -> { 
+				currentCalc.append(strings[buttonID]);
+				screen.setText(currentCalc.toString());
+			});
+		}
 		
-		//FUNCTIONS		
+		buttons[20].setText("\u221A(x)");
+		buttons[21].setText("x^y");
+		buttons[22].setText("e^x");
+		buttons[23].setText("sin(x)");
+		buttons[24].setText("cos(x)");
+				
 		Button toggle = new Button("Mode");
-		Button answer = new Button("ANS");
 		Button clear = new Button("C");
 		Button equals = new Button("=");
-		Button decimal = new Button(".");
-		Button negative = new Button("-");
-		Button openP = new Button("(");
-		Button closeP = new Button(")");
 		
-		Button addition= new Button("+");
-		Button subtraction = new Button("-");
-		Button multiplication = new Button("*");
-		Button division = new Button("/");
-		Button log = new Button("log");
-		Button sqrt = new Button(String.format("%c", (char) 251));
-		Button power = new Button("x^y");
-		Button exponential = new Button("e^x");
-		Button sine = new Button("sin(x)");
-		Button cosine = new Button("cos(x)");
-		
+		toggle.setMinSize(300, 50);
+		clear.setMinSize(150, 50);
+		equals.setMinSize(600,50);	
+				
 		//PANES IN ORDER**********************************
 		VBox base = new VBox();
 		base.setAlignment(Pos.CENTER);
-		
-		Text screen = new Text("NULL");
 		HBox ansCandToggle = new HBox();
 		
 		VBox science = new VBox();
@@ -73,40 +75,44 @@ public class CalculatorGUI extends Application{
 
 		//BUTTON & TEXT ASSEMBLY**************************
 		base.getChildren().add(screen);
-		
-		ansCandToggle.getChildren().add(answer);
-		ansCandToggle.getChildren().add(clear);
+		screen.setX(100);
+		screen.setY(100);
+		//ANS,TOGGLE,CLEAR
+		ansCandToggle.getChildren().add(buttons[10]);
 		ansCandToggle.getChildren().add(toggle);
+		ansCandToggle.getChildren().add(clear);
 		
-		sciencer1.getChildren().add(log);
-		sciencer1.getChildren().add(sqrt);
-		sciencer1.getChildren().add(openP);
-		sciencer1.getChildren().add(closeP);
+		//E^X,LOG,(,)
+		sciencer1.getChildren().add(buttons[22]);
+		sciencer1.getChildren().add(buttons[19]);
+		sciencer1.getChildren().add(buttons[13]);
+		sciencer1.getChildren().add(buttons[14]);
 		
-		sciencer2.getChildren().add(sine);
-		sciencer2.getChildren().add(cosine);
-		sciencer2.getChildren().add(power);
-		sciencer2.getChildren().add(exponential);
+		//X^Y,SQRT,SIN,COS
+		sciencer2.getChildren().add(buttons[21]);
+		sciencer2.getChildren().add(buttons[20]);
+		sciencer2.getChildren().add(buttons[23]);
+		sciencer2.getChildren().add(buttons[24]);
 		
-		mainr1.getChildren().add(seven);
-		mainr1.getChildren().add(eight);
-		mainr1.getChildren().add(nine);
-		mainr1.getChildren().add(addition);
+		mainr1.getChildren().add(buttons[7]);
+		mainr1.getChildren().add(buttons[8]);
+		mainr1.getChildren().add(buttons[9]);
+		mainr1.getChildren().add(buttons[15]);
 		
-		mainr2.getChildren().add(four);
-		mainr2.getChildren().add(five);
-		mainr2.getChildren().add(six);
-		mainr2.getChildren().add(subtraction);
+		mainr2.getChildren().add(buttons[4]);
+		mainr2.getChildren().add(buttons[5]);
+		mainr2.getChildren().add(buttons[6]);
+		mainr2.getChildren().add(buttons[16]);
 		
-		mainr3.getChildren().add(one);
-		mainr3.getChildren().add(two);
-		mainr3.getChildren().add(three);
-		mainr3.getChildren().add(multiplication);
+		mainr3.getChildren().add(buttons[1]);
+		mainr3.getChildren().add(buttons[2]);
+		mainr3.getChildren().add(buttons[3]);
+		mainr3.getChildren().add(buttons[17]);
 		
-		mainr4.getChildren().add(decimal);
-		mainr4.getChildren().add(zero);
-		mainr4.getChildren().add(negative);
-		mainr4.getChildren().add(division);
+		mainr4.getChildren().add(buttons[11]);
+		mainr4.getChildren().add(buttons[0]);
+		mainr4.getChildren().add(buttons[12]);
+		mainr4.getChildren().add(buttons[18]);
 		
 		
 		
@@ -121,6 +127,14 @@ public class CalculatorGUI extends Application{
 		main.getChildren().add(mainr4);
 		main.getChildren().add(equals);
 		
+		//OUTPUT WINDOW****************
+		Stage output = new Stage();		
+		output.setTitle("Calculations");
+		VBox outBox = new VBox();
+		output.setScene(new Scene(outBox, 500, 220));
+		output.show();
+		
+		//ACTION EVENTS****************		
 		toggle.setOnAction(event -> {
 			if(!this.scientific) {
 				primaryStage.setTitle("Scientific Calculator");
@@ -137,11 +151,27 @@ public class CalculatorGUI extends Application{
 			}
 		});
 		
+		clear.setOnAction(event -> {
+			currentCalc.delete(0, currentCalc.length());
+			screen.setText(currentCalc.toString());
+		});
+		
+		equals.setOnAction(event -> {
+			if(storage.getArr().size() == 10) {
+				outBox.getChildren().remove(0);
+				storage.removeFirst();
+			}
+			storage.setResult(currentCalc.toString());
+			outBox.getChildren().add(new Text(currentCalc.toString()));
+			currentCalc.delete(0, currentCalc.length());
+			screen.setText(currentCalc.toString());
+		});
+		
 		primaryStage.setTitle("Basic Calculator");
-		primaryStage.setScene(new Scene(base, 200, 200));
+		primaryStage.setScene(new Scene(base, 600, 450));
 		primaryStage.show();
-		
-		
 	}
 
 }
+
+
